20bff359c2bf3bb6622a00e25e66e13211bbbdb4
Avika Trivedi <84050503+AvikaTrivedi@users.noreply.github.com>
2022-01-03 09:23:47 +0530

    :)

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
-git clone -b dev https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
-cp ultroid/.env /root/TeamUltroid/.env
-cd /root/TeamUltroid
+git clone -b dev https://github.com/TeamUltroid/Ultroid
+cp ultroid/.env Ultroid/.env
+cd Ultroid
 docker build . -t ultroid
 docker run -i ultroid
