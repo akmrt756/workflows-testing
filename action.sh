07d1c83149c2779792e5c73cc83852a077370125
Avika Trivedi <84050503+AvikaTrivedi@users.noreply.github.com>
2022-01-20 20:39:55 +0530

    Huh

--- action.sh
+++ action.sh
@@ -2,8 +2,9 @@ apt-get update && apt-get upgrade
 git clone -b dev https://github.com/TeamUltroid/Ultroid 
 chmod 777 Ultroid
 cp SedLoif/.env Ultroid/.env
-cd Ultroid
+cd Ultroid 
 pip3 install --no-cache-dir -r requirements.txt 
 pip3 install --no-cache-dir -r res*/sta*/opt*.txt
+pip3 install youtube_dl -U
 pip3 install av --no-binary av
 python3 -m pyUltroid
