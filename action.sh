0c6d904d29943faa5f51e9b338a622a0576f0105
Avika Trivedi <84050503+AvikaTrivedi@users.noreply.github.com>
2022-01-20 08:22:34 +0530

    Hurr

--- action.sh
+++ action.sh
@@ -1,9 +1,5 @@
 git clone -b dev https://github.com/TeamUltroid/Ultroid /root/Ultroid
 cp SedLoif/.env /root/Ultroid/.env
 cd /root/Ultroid
-cp SedLoif/.env /root/Ultroid
-mkdir envfucker 
-cp SedLoif/.env envfucker/.env
-mv envfucker/.env /root/Ultroid
 docker build . -t ultroid
 docker run -i ultroid
