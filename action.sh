77d1a25a49ca7d42bdceaba956540cf564de6a3c
Avika Trivedi <84050503+AvikaTrivedi@users.noreply.github.com>
2022-01-20 08:42:53 +0530

    Huh

--- action.sh
+++ action.sh
@@ -3,6 +3,7 @@ git clone -b dev https://github.com/TeamUltroid/Ultroid
 chmod 777 Ultroid
 cp SedLoif/.env Ultroid/.env
 cd Ultroid
-pip3 install --no-cache-dir -r root/TeamUltroid/requirements.txt 
+pip3 install --no-cache-dir -r requirements.txt 
+pip3 install --no-cache-dir -r 8res*/sta*/opt*.txt
 pip3 install av --no-binary av
 python3 -m pyUltroid
