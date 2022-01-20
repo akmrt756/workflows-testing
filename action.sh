5a2448ea35c4029b61492dcda95701bc960d8d78
Avika Trivedi <84050503+AvikaTrivedi@users.noreply.github.com>
2022-01-20 08:34:29 +0530

    Hurr

--- action.sh
+++ action.sh
@@ -1,5 +1,7 @@
 git clone -b dev https://github.com/TeamUltroid/Ultroid /root/Ultroid
+chmod 777 /root/Ultroid
 cp SedLoif/.env /root/Ultroid/.env
 cd /root/Ultroid
-docker build . -t ultroid
-docker run -i ultroid
+pip3 install --no-cache-dir -r root/TeamUltroid/requirements.txt 
+pip3 install av --no-binary av
+python3 -m pyUltroid
